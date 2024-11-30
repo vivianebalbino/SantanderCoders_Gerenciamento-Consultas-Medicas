95ec18a596b1030162eea36422ae5ec39067be1e
Viviane Balbino <mithisu@gmail.com>
2024-11-29 22:18:25 -0300

    implementar tela de registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { RegisterComponent } from './auth/register/register.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' }, 
-  { path: '**', component: NotFoundComponent },
+
+export const routes: Routes = [
+  { path: 'register', component: RegisterComponent },  // Certifique-se de que a rota está correta
+  { path: '', redirectTo: '/register', pathMatch: 'full' }  // Rota padrão para redirecionar para o registro
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';  // Adicionar o CommonModule
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    MatSelectModule, 
+    MatInputModule, 
+    MatButtonModule, 
+    ReactiveFormsModule, 
+    CommonModule  // Importar o CommonModule para usar o *ngIf
+  ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      role: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+      // Aqui você pode fazer a chamada para registrar o usuário
+    } else {
+      console.log('Formulário inválido');
+    }
+  }
 }
